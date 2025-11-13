15e82b6275c67824613ab7842bd6e7487dc86eb8
sazz-dev <sazzadur.rahman193@gmail.com>
2025-11-13 23:01:43 +0600

    env update

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     //Get Data from mongodb
 
@@ -181,7 +181,7 @@ async function run() {
       res.send(result);
     });
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
